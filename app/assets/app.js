5df6dc13808a22cd68e05f366d83c93c12747308
Cyril David <cdhinho27370@outlook.fr>
2026-05-21 17:33:09 +0200

    BP-27 : continuation du ticket BP-27

--- app/assets/app.js
+++ app/assets/app.js
@@ -1,13 +1,8 @@
-import './stimulus_bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
+// Point d'entrée principal — importe les modules JS et CSS
 import './styles/app.css';
-import './header.js';
-import './nav.js';
-import './main.js';
-import './aside.js';
-import './footer.js';
+import './stimulus_bootstrap.js';
+//import '.js/header.js';
+//import '.js/nav.js';
+//import '.js/main.js';
+//import '.js/aside.js';
+//import '.js/footer.js';
