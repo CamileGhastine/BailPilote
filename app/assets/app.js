9a5f6e3fe5054db6c882fd8c79cde9b1fbb8070d
Cyril David <cdhinho27370@outlook.fr>
2026-05-21 13:53:22 +0200

    creation du HomeController, des fichiers css et js ainsi que les templates twig

--- app/assets/app.js
+++ app/assets/app.js
@@ -6,5 +6,8 @@ import './stimulus_bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import './header.js';
+import './nav.js';
+import './main.js';
+import './aside.js';
+import './footer.js';
