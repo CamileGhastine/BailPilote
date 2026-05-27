2d262c64784661e26aac1cc44ad8d04db3c3e0fa
MohandBir <birmoho@gmail.com>
2026-05-27 16:45:06 +0200

    BP-63: logout

--- app/src/Controller/TemplateController.php
+++ app/src/Controller/TemplateController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class TemplateController extends AbstractController
 {
-    #[Route('/', name: 'app_template')]
+    #[Route('/', name: 'app_template_index')]
     public function index(): Response
     {
         return $this->render('template/index.html.twig', [
