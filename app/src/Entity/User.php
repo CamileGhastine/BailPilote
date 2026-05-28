2839be3de8a13e5b5bf727aee5fc6860e4265972
CamileGhastine <camileghastine@hotmail.com>
2026-05-28 17:34:33 +0200

    fix bug

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -133,30 +133,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         // @deprecated, to be removed when upgrading to Symfony 8
     }
 
-    public function getFirstname(): ?string
-    {
-        return $this->firstname;
-    }
-
-    public function setFirstname(string $firstname): static
-    {
-        $this->firstname = $firstname;
-
-        return $this;
-    }
-
-    public function getLastname(): ?string
-    {
-        return $this->lastname;
-    }
-
-    public function setLastname(string $lastname): static
-    {
-        $this->lastname = $lastname;
-
-        return $this;
-    }
-
     public function getRegistrationToken(): ?string
     {
         return $this->registration_token;
