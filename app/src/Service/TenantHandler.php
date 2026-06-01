e3ffb6ac06ddf92624c006e8381981c1b8e59894
Cyril David <cdhinho27370@outlook.fr>
2026-06-01 14:28:08 +0200

    BP-36 : Création du add_tenant.html.twig

--- app/src/Service/TenantHandler.php
+++ app/src/Service/TenantHandler.php
@@ -1,4 +1,7 @@
 <?php
+
+namespace App\Service;
+
 use App\Entity\Lease;
 use App\Entity\Tenant;
 use App\Entity\User;
@@ -9,33 +12,31 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class TenantHandler
 {
     public function __construct(
-      private EntityManagerInterface $em,
-      private UserPasswordHasherInterface $passwordHasher
-    )
-    {}
+        private EntityManagerInterface $em,
+        private UserPasswordHasherInterface $passwordHasher,
+    ) {}
 
     public function createTenantFromForm(FormInterface $form, Lease $lease): void
-    {}
-    
+    {
         $user = new User();
-        $user->setFirstName($form->get('firstName')->getData());
-        $user->setLastName($form->get('lastName')->getData());
+        $user->setFirstname($form->get('firstname')->getData());
+        $user->setLastname($form->get('lastname')->getData());
         $user->setEmail($form->get('email')->getData());
         $user->setPhone($form->get('phone')->getData());
         $user->setRoles(['ROLE_TENANT']);
         $user->setIsVerified(false);
-        
+
         $hashedPassword = $this->passwordHasher->hashPassword(
-                $user,
-                $form->get('password')->getData()
-            );
+            $user,
+            $form->get('password')->getData()
+        );
         $user->setPassword($hashedPassword);
 
         $tenant = new Tenant();
         $tenant->setUser($user);
         $tenant->setLease($lease);
 
-        $this->em->persist($user);
         $this->em->persist($tenant);
         $this->em->flush();
     }
+}
